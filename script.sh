9f7205b992bfed6f5807c2859072c4b37ec6deff
Nicola PAOLETTI <npaolett@f1r1s14.clusters.42paris.fr>
2025-12-12 17:01:22 +0100

    1 try done:

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while read path; do
-  code=$(curl -s -o /dev/null -w "%{http_code}" -k https://192.168.56.101:443/$path)
+  code=$(curl -s -o /dev/null -w "%{http_code}" -k https://$2/$path)
   if [ "$code" == "200" ] || [ "$code" == "301" ] || [ "$code" == "302" ]; then
     echo "$code - /$path"
-    firefox https://192.168.56.101:443/$path/ &
+    firefox https://$2/$path/ &
     sleep 1
   fi
 done < "$1"
